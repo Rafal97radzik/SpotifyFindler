40b840fffa30458868cef7b8f10af78c417844fe
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-03-15 21:34:12 +0100

    Dodanie SpotifyWebClient

---
+++ SpotifyFindler/SpotifyAPI/Web/SpotifyWebClient.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyAPI.Web
+{
+    class SpotifyWebClient
+    {
+        public async Task<string> GetDataFromRequestAsync(string requestUri, HttpRequestHeader headerType, string headerValue, string postData)
+        {
+            return await GetDataFromRequestAsync(requestUri, headerType, headerValue, postData);
+        }
+
+        public async Task<string> GetDataFromRequestAsync(string requestUri, string postData)
+        {
+            return await GetDataFromRequestAsync(requestUri, null, null, postData);
+        }
+
+        public async Task<string> GetDataFromRequestAsync(string requestUri, HttpRequestHeader headerType, string headerValue)
+        {
+            return await GetDataFromRequestAsync(requestUri, headerType, headerValue, null);
+        }
+
+        public async Task<string> GetDataFromRequestAsync(string requestUri)
+        {
+            return await GetDataFromRequestAsync(requestUri, null, null, null);
+        }
+
+
+        public string GetDataFromRequest(string requestUri, HttpRequestHeader headerType, string headerValue, string postData)
+        {
+            return GetDataFromRequest(requestUri, headerType, headerValue, postData);
+        }
+
+        public string GetDataFromRequest(string requestUri, string postData)
+        {
+            return GetDataFromRequest(requestUri, null, null, postData);
+        }
+
+        public string GetDataFromRequest(string requestUri, HttpRequestHeader headerType, string headerValue)
+        {
+            return GetDataFromRequest(requestUri, headerType, headerValue, null);
+        }
+
+        public string GetDataFromRequest(string requestUri)
+        {
+            return GetDataFromRequest(requestUri, null, null, null);
+        }
+
+
+        private async Task<string> GetDataFromRequestAsync(string requestUri, HttpRequestHeader? headerType, string headerValue, string postData)
+        {
+            WebResponse response = await GetResponseAsync(requestUri, headerType, headerValue, postData);
+
+            using (var responseReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return await responseReader.ReadToEndAsync();
+            }
+        }
+
+        private string GetDataFromRequest(string requestUri, HttpRequestHeader? headerType, string headerValue, string postData)
+        {
+            WebResponse response =  GetResponse(requestUri, headerType, headerValue, postData);
+
+            using (var responseReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return responseReader.ReadToEnd();
+            }
+        }
+
+        private async Task<WebResponse> GetResponseAsync(string requestUri, HttpRequestHeader? headerType, string headerValue, string postData)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(requestUri);
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (headerType.HasValue)
+                request.Headers.Add(headerType.Value, headerValue);
+
+            if (string.IsNullOrEmpty(postData))
+            {
+                request.Method = WebRequestMethods.Http.Get;
+            }
+            else
+            {
+                request.Method = WebRequestMethods.Http.Post;
+                SendPostData(postData, request);
+            }
+
+            return await request.GetResponseAsync();
+        }
+
+        private WebResponse GetResponse(string requestUri, HttpRequestHeader? headerType, string headerValue, string postData)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(requestUri);
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (headerType.HasValue)
+                request.Headers.Add(headerType.Value, headerValue);
+
+            if (string.IsNullOrEmpty(postData))
+            {
+                request.Method = WebRequestMethods.Http.Get;
+            }
+            else
+            {
+                request.Method = WebRequestMethods.Http.Post;
+                SendPostData(postData, request);
+            }
+
+            return request.GetResponse();
+        }
+
+        private static void SendPostData(string postData, HttpWebRequest request)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            request.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+        }
+    }
+}
