1f8298b86ff8ca94eabce8bbc871b73f187a132a
RafałRadzik <40192818+Rafal97radzik@users.noreply.github.com>
2020-03-17 10:49:40 +0100

    Dodanie SearchViewModel

---
+++ SpotifyFindler/SpotifyFindler/ViewModels/SearchViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI.Models;
+
+namespace SpotifyFindler.ViewModels
+{
+    public class SearchViewModel : INotifyPropertyChanged
+    {
+        private Search searchResult = new Search();
+
+        public Search SearchResult
+        {
+            get { return searchResult; }
+            set { searchResult = value; OnPropertyChanged(new PropertyChangedEventArgs("SearchResult")); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
